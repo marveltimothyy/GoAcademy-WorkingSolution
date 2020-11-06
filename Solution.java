35b7cb95a2954122c0289e246d9427ded22bad1c
Marvel Manullang <marvel.manullang10@gmail.com>
2020-11-06 18:42:24 +0800

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,39 @@
+import java.lang.reflect.Array;
+import java.util.*;
+public class Solution {
+
+    public static void main(String[] args) {
+//        System.out.println("Hello World");
+        Scanner sc = new Scanner(System.in);
+        String inpt;
+        String [] dat = new String[2];
+        float res= 0 ;
+        do {
+            inpt = sc.nextLine().toLowerCase();
+            dat = inpt.split(" ");
+            if (!dat[0].equals("exit")) {
+                if ((dat.length) == 2) {
+                    if (dat[0].equals("add")) {
+                        res += Integer.parseInt(dat[1]);
+                    } else if (dat[0].equals("substract")) {
+                        res -= Integer.parseInt(dat[1]);
+                    } else if (dat[0].equals("multiply")) {
+                        res *= Integer.parseInt(dat[1]);
+                    } else if (dat[0].equals("divide")) {
+                        if (dat[1].equals('0')) {
+                            res = 0;
+                        } else {
+                            res /= Integer.parseInt(dat[1]);
+                        }
+                    }
+                }
+            else if(dat[0].equals("cancle")){
+                res = 0;
+            }
+                System.out.printf("%,.1f", res);
+                System.out.println("");
+            }
+        }while((!dat[0].equals("exit")));
+//        System.out.println(Arrays.toString(dat));
+    }
+}
